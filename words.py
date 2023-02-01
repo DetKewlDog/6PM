e4337b142257ca95bf78749c0a1aa2431bfe8384
DetKewlDog <79573375+DetKewlDog@users.noreply.github.com>
2023-02-01 11:25:57 +0000

    ok it didnt actually update the repo apparently

--- words.py
+++ words.py
@@ -1,6 +1,18 @@
 # Wordlists for the bot to check for
 # More obscure words are at the bottom (seperated by a newline)
 good_words = set([
+  'beautiful', 'attractive', 'sexy', 'good', 'pog', 'cool', 'nice',
+  'legendary', 'great', 'neat', 'best', 'brilliant', 'incredible ',
+  'fantastic', 'godly', 'lovely', 'pretty', 'perfect', 'neat', 'smart',
+  'super', 'sweet', 'wholesome', 'wise', 'swag', 'poggers', 'clever',
+  'conscious', 'cute', 'funny', 'fun', 'friendly', 'flattering', 'glorious',
+  'helpful', 'inspiring', 'intelligent', 'kind', 'marvellous', 'pleasant',
+  'polite', 'perceptive', 'patient', 'praiseworthy', 'rational', 'self aware',
+  'sensational', 'sensible', 'sharp', 'strong', 'spectacular', 'stunning',
+  'superior', 'supportive', 'thoughtful', 'trustworthy', 'unparalleled',
+  'valuable', 'vigilant', 'well mannered', 'wonderful', 'captivating',
+  'magnificent', 'breathtaking', 'splendid', 'stellar', 'epic', 'sigma',
+=======
   'beautiful',
   'attractive',
   'sexy',
@@ -69,6 +81,7 @@ good_words = set([
   'stellar',
   'epic',
   'sigma',
+  'alpha'
 ])
 
 neutral_words = set(['special', 'drunk', 'faulty', 'confusing', 'goofy ahh'])
@@ -81,8 +94,7 @@ bad_words = set([
   'concerning', 'convoluted', 'corny', 'corrupt', 'cowardly', 'costly',
   'crazy', 'daft', 'dangerous', 'defective', 'deformed', 'dense', 'dirty',
   'disgusting', 'empty', 'faulty', 'idiotic', 'moody', 'nosy', 'petty',
-  'pitiful', 'tacky', 'ballsucking', 'imbecile', 'crybaby', 'no bitches',
-  'baby'
+  'pitiful', 'tacky', 'ballsucking', 'imbecile', 'crybaby', 'no bitches', 'baby'
 ])
 
 evil_words = set([
